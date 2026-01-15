7a2dee995224990292d673b838fec46a31aa8d5a
neel c <neilchaudhary12@gmail.com>
2026-01-15 19:05:03 +0530

    feat: fetching db vals from env class

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,13 +3,14 @@ import type { Document as MongoDBDoc } from 'mongodb';
 import { Logger } from './logger.ts';
 import { isNullOrUndefined } from './predicates.ts';
 import { getErrorDetails, SidekickPlatformError } from './exceptions.ts';
+import { Env } from './env.ts';
 
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 const logger = new Logger('database');
-const mongoURI = process.env.mongoURI;
+const mongoURI = Env.get('mongoURI');
 export const DB_NAME = 'sidekick-platform';
 export const usersCollection = 'users';
 
@@ -26,7 +27,7 @@ class DB {
       logger.info(`Connecting to MongoDB...`);
       DB.client = await new MongoClient(mongoURI!, { tls: true }).connect();
       DB.users = DB.client.db(DB_NAME).collection(usersCollection);
-      logger.info('beacon-core DB Connection Initialised');
+      logger.info('Sidekick-Platform DB Connection Initialised');
     } catch (error) {
       logger.error('Failed to connect to MongoDB due to : ', getErrorDetails(error));
       throw SidekickPlatformError.database('Failed to connect to MongoDB');
