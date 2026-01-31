c4f98f5d4bbd7e3c1b4aefeee03e0d4a0a5b3bb8
neel c <neilchaudhary12@gmail.com>
2026-02-01 04:34:56 +0530

    feat: ratelimiter middleware

--- src/config/middlewares.ts
+++ src/config/middlewares.ts
@@ -3,7 +3,7 @@ import { Ratelimiter } from './ratelimiter.ts';
 
 const ratelimiter = new Ratelimiter();
 
-export const checkRateLimit = (req: Request, res: Response, next: NextFunction) => {
+export const checkRateLimit = (_req: Request, res: Response, next: NextFunction) => {
   if (ratelimiter.allowRequest()) {
     next();
   } else {
