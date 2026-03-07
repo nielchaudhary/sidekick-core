12134e9c5f58a981e32c91242bf5004a97b184ce
neel c <neilchaudhary12@gmail.com>
2026-03-07 23:33:00 +0530

    feat: claude

--- src/config/openai.ts
+++ src/config/openai.ts
@@ -1,22 +1,22 @@
 import OpenAI from 'openai';
-import superagent from 'superagent';
 import { Env } from './env.ts';
-import { Readable } from 'stream';
 import { Logger } from './logger.ts';
 
 const OPENAI_API_KEY = Env.get('OPENAI_API_KEY');
 
 const logger = new Logger('openai');
-const openAIClient = new OpenAI({
+export const openAIClient = new OpenAI({
   apiKey: OPENAI_API_KEY,
 });
 
-export const transcribeAudio = async (audioUrl: string) => {
-  try {
-    // Download audio using superagent
-    // use openai / groq for transcription
-  } catch (error) {
-    logger.error('Transcription failed:', error);
-    throw error;
-  }
-};
+logger.info(`Inside OpenAI client`);
+
+// export const transcribeAudio = async (audioUrl: string) => {
+//   try {
+//     // Download audio using superagent
+//     // use openai / groq for transcription
+//   } catch (error) {
+//     logger.error('Transcription failed:', error);
+//     throw error;
+//   }
+// };
