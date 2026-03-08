99fc25c3a10c7d3efff6f850d1b9277ea11b1e28
neel c <neilchaudhary12@gmail.com>
2026-03-08 23:50:05 +0530

    feat: chat router

---
+++ src/services/chat/chatRouterV1.ts
@@ -0,0 +1,8 @@
+import express, { type Router } from 'express';
+import { streamTextPostHandler } from './api/streamTextPostHandler.ts';
+
+const chatRouter = express.Router();
+
+chatRouter.post('/stream', streamTextPostHandler);
+
+export const chatRouterV1: [string, Router] = ['/v1/chat', chatRouter];
