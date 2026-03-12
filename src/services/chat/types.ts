0ca53dfef8dbc3c311566281703965018983b570
neel c <neilchaudhary12@gmail.com>
2026-03-12 17:41:15 +0530

    feat: messages interface, backend integration

--- src/services/chat/types.ts
+++ src/services/chat/types.ts
@@ -1,1 +1,20 @@
-export interface Chat {}
+export interface IChat {
+  sessionId: string;
+  messages: IMessage[];
+  ctime: Date; //on the whole conversation level
+}
+
+export enum RoleTypes {
+  USER = 'user',
+  SYSTEM = 'system',
+}
+
+export interface IMessage {
+  messageId: string;
+  role: RoleTypes;
+  message: string;
+  ctime: Date; //granular data on message level
+  like: boolean;
+  dislike: boolean;
+  copied: boolean;
+}
