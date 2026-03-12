9d9aa81710696f32043c0641df51a216188e6d3a
neel c <neilchaudhary12@gmail.com>
2026-03-12 22:18:57 +0530

    feat: add temp fallback LLM for costs

--- src/providers/anthropic.ts
+++ src/providers/anthropic.ts
@@ -30,30 +30,29 @@ export const streamTextUsingAnthropic = async (
       ? `${SIDEKICK_DEFAULT_PROMPT}\n\n${systemPrompt}`
       : SIDEKICK_DEFAULT_PROMPT;
 
-    // const stream = anthropicClient.messages.stream({
-    //   model: 'claude-sonnet-4-5-20250929',
-    //   max_tokens: 1024,
-    //   system: finalSystemPrompt,
-    //   tools: [
-    //     {
-    //       type: 'web_search_20250305',
-    //       name: 'web_search',
-    //       max_uses: 1,
-    //     },
-    //   ],
-    //   messages: [{ role: RoleTypes.USER, content: prompt }],
-    // cache_control: { type: 'ephemeral' },
-
-    // });
-
-    //cheap one for now
     const stream = anthropicClient.messages.stream({
-      model: 'claude-haiku-4-5-20251001',
-      max_tokens: 512,
+      model: 'claude-sonnet-4-5-20250929',
+      max_tokens: 1024,
       system: finalSystemPrompt,
+      tools: [
+        {
+          type: 'web_search_20250305',
+          name: 'web_search',
+          max_uses: 1,
+        },
+      ],
       messages: [{ role: RoleTypes.USER, content: prompt }],
       cache_control: { type: 'ephemeral' },
     });
+
+    // //cheap one for now
+    // const stream = anthropicClient.messages.stream({
+    //   model: 'claude-haiku-4-5-20251001',
+    //   max_tokens: 512,
+    //   system: finalSystemPrompt,
+    //   messages: [{ role: RoleTypes.USER, content: prompt }],
+    //   cache_control: { type: 'ephemeral' },
+    // });
 
     for await (const event of stream) {
       if (
