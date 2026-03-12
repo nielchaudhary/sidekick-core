548c1c18b623c9d133afa26280031472b6dd8e79
neel c <neilchaudhary12@gmail.com>
2026-03-13 00:24:03 +0530

    feat: messages

--- src/services/chat/types.ts
+++ src/services/chat/types.ts
@@ -1,8 +1,10 @@
 //document insertion
 export interface IChat {
   sessionId: string;
+  title: string;
   messages: IMessage[];
   ctime: Date; //on the whole conversation level
+  model: string;
 }
 
 export enum RoleTypes {
@@ -11,9 +13,9 @@ export enum RoleTypes {
 }
 
 export interface IMessage {
-  messageId: string;
+  contentId: string;
   role: RoleTypes;
-  message: string;
+  content: string;
   ctime: Date; //granular data on message level
   like: boolean;
   dislike: boolean;
